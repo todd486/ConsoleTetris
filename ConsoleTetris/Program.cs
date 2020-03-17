9c61a6135f14001ac6d7a4d049f25b74c2d66564
todd486 <58509177+todd486@users.noreply.github.com>
2020-03-17 10:31:58 +0100

    semi works again

--- ConsoleTetris/Program.cs
+++ ConsoleTetris/Program.cs
@@ -1,14 +1,10 @@
 ﻿using System;
 using System.Threading;
-using System.Collections.Generic;
 using System.Linq;
 
-namespace ConsoleTetris
-{
-    class Program
-    {
-        public class Tetromino
-        {
+namespace ConsoleTetris {
+    class Program {
+        public class Tetromino {
             public Tetromino() { }
             private byte[][,] data = { //jagged array storing shape info in hex, to later be converted into binary
                 new byte[,] { { 0x0 }, { 0xF } },
@@ -20,8 +16,8 @@ namespace ConsoleTetris
                 new byte[,] { { 0xC }, { 0x6 } }
             };
             private int[] Coords = { 0, 0 };
-            public int Xpos { get => Coords[0]; set { Coords[0] = (value >= 0 && value < 10 ? value : 0); } }
-            public int Ypos { get => Coords[1]; set { Coords[1] = (value >= 0 && value < 15 ? value : 0); } }
+            public int Xpos { get => Coords[0]; set { Coords[0] = (value >= 0 && value < 10 ? value : Coords[0]); } }
+            public int Ypos { get => Coords[1]; set { Coords[1] = (value >= 0 && value < 15 ? value : Coords[1]); } }
             public int[,] Shape = new int[4, 4];
             public void genShape(int dataIndex) {
                 int[,] temp = new int[4, 4];
@@ -47,24 +43,19 @@ namespace ConsoleTetris
             bool active = false;
             int difficulty = 1;
             int[,] field = new int[10, 16];
+
             Tetromino current = new Tetromino(); //instantiate a tetromino
             Random random = new Random(); //instantiate a random
             Console.CursorVisible = false;
-            bool Occupied(int x, int y) /*TODO: Implement collision detection */ {
-                return field[x, y] != 0 ? false : true;
-            }
-            void Rotate(int direction) {
-                switch (direction) {
-                    case 0: {
 
-                            break;
-                        }
-                    case 1: {
-
-                            break;
-                        }
-                    default: { break; }
+            void Rotate(int direction) { //doesn't really work but ok
+                int[,] temp = new int[4, 4];
+                temp = current.Shape; 
+                for (int i = 0; i < 4; i++) {for (int j = 0; j < 4; j++) { //https://stackoverflow.com/a/42535
+                    temp[i, j] = current.Shape[4 - j - 1, i];
+                    }
                 }
+                current.Shape = temp;
             }
             void Controls() {
                 while (true) {
@@ -75,59 +66,54 @@ namespace ConsoleTetris
                             case ConsoleKey.DownArrow: { current.Ypos++; break; }
                             case ConsoleKey.LeftArrow: { current.Xpos--; break; }
                             case ConsoleKey.RightArrow: { current.Xpos++; break; }
-                            case ConsoleKey.Z: { /*Rotate(0)*/; break; }
-                            case ConsoleKey.X: { /*Rotate(1);*/ break; }
+                            case ConsoleKey.Z: { Rotate(0); break; }
+                            case ConsoleKey.X: { Rotate(1); break; }
                             case ConsoleKey.Escape: { State.paused = !State.paused; break; }
                             default: { break; }
                         }
                     }
                 }
             }
-            void Cascade() {
-                int acc = 0;
-                int i = 0;
-                for (int y = 0; y < 16; y++) {
-                    for (int x = 0; x < 10; x++) {
-                        acc = field[x, y];
-                    }
-                    i++;
-                    acc = 0;
-                }
-            }
+            //void Cascade() {
+            //    int acc = 0;
+            //    int i = 0;
+            //    for (int y = 0; y < 16; y++) {
+            //        for (int x = 0; x < 10; x++) {
+            //            acc = field[x, y];
+            //        }
+            //        i++;
+            //        acc = 0;
+            //    }
+            //}
             
             Thread drawThread = new Thread(() => Draw(field, current));
             Thread controlThread = new Thread(() => Controls());
             
             drawThread.Start();
             controlThread.Start();
-            while (!State.lost) { //TODO: create a new tetromino instead of mutating original, apply it to array in set statement, store X & Y somewhere efficiently
-                if (!State.paused) {
-                    Thread.Sleep(1000 / difficulty); //speed up later to up difficulty
-                    current.genShape(random.Next(6));
-                    //if (!active) {
-                        
-                    //}
-                    
-                    //if (current.Ypos <= 15) { current.Ypos++; }
-                    //else {
-                    //    active = false;
-                    //    for (int y = 0; y < 4; y++) { for (int x = 0; x < 4; x++) {
-                    //            if (current.setShape[x, y] == 0) {
-                    //                field[current.Xpos, current.Ypos] = current.setShape[x, y];
-                    //            }
-                    //        } //push the current tetromino piece into field & only modify the field if there is anything there
-                    //    }
-                    //}
-                    //Cascade();
-                }
-                //if (!active) { //check if a tetromino is active
-                //    active = true;
-                //    current.Xpos = 0;
-                //    current.Ypos = 0;
-                //    current.setShape = current.getShape(random.Next(6)); //if not, set a new shape for the tetromino
-                //}
 
-                
+            while (!State.lost) {
+                if (!State.paused) {
+                    if (!active) {
+                        active = true;
+                        current.genShape(random.Next(6));
+                    } else {
+                        if (current.Ypos + 1 != 15) {
+                            current.Ypos++;
+                        } else {
+                            active = false;
+                            for (int y = 0; y < 4; y++) { for (int x = 0; x < 4; x++) { 
+                                    if (current.Shape[x, y] != 0) {
+                                        field[(current.Xpos + x >= 0 && current.Xpos + x <= 9 ? current.Xpos + x : 0), 
+                                            (current.Ypos + y >= 0 && current.Ypos + y <= 15 ? current.Ypos + y : 0)] = current.Shape[x, y];
+                                    }
+                                } 
+                            }
+                            current.Xpos = 0; current.Ypos = 0;
+                        }
+                    }
+                    Thread.Sleep(1000 / difficulty); //speed up later to up difficulty
+                }
             }
             drawThread.Join();
             controlThread.Join();

feb0e0f33f84161c58079af7a7ed4dbeea1afdff
todd486 <filip.svahn@elev.ga.ntig.se>
2020-03-19 11:52:11 +0100

    minor restructuring

--- ConsoleTetris/Program.cs
+++ ConsoleTetris/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleTetris {
     class Program {
         public class Tetromino {
             public Tetromino() { }
-            private byte[][,] data = { //jagged array storing shape info in hex, to later be converted into binary
+            private readonly byte[][,] data = { //jagged array storing shape info in hex, to later be converted into binary
                 new byte[,] { { 0x0 }, { 0xF } },
                 new byte[,] { { 0x8 }, { 0xE } },
                 new byte[,] { { 0x2 }, { 0xE } },
@@ -19,7 +19,7 @@ namespace ConsoleTetris {
             public int Xpos { get => Coords[0]; set { Coords[0] = (value >= 0 && value < 10 ? value : Coords[0]); } }
             public int Ypos { get => Coords[1]; set { Coords[1] = (value >= 0 && value < 15 ? value : Coords[1]); } }
             public int[,] Shape = new int[4, 4];
-            public void genShape(int dataIndex) {
+            public void GenShape(int dataIndex) {
                 int[,] temp = new int[4, 4];
                 int acc = 0;
                 foreach (byte shape in data[dataIndex]) {
@@ -36,8 +36,8 @@ namespace ConsoleTetris {
             }
         }
         public static class State {
-            public static bool paused { get; set; }
-            public static bool lost { get; set; }
+            public static bool Paused { get; set; }
+            public static bool Lost { get; set; }
         }
         static void Main(string[] args) {
             bool active = false;
@@ -50,9 +50,10 @@ namespace ConsoleTetris {
 
             void Rotate(int direction) { //doesn't really work but ok
                 int[,] temp = new int[4, 4];
-                temp = current.Shape; 
-                for (int i = 0; i < 4; i++) {for (int j = 0; j < 4; j++) { //https://stackoverflow.com/a/42535
-                    temp[i, j] = current.Shape[4 - j - 1, i];
+                temp = current.Shape;
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 4; j++) { //https://stackoverflow.com/a/42535
+                        temp[i, j] = current.Shape[j, i];
                     }
                 }
                 current.Shape = temp;
@@ -68,7 +69,7 @@ namespace ConsoleTetris {
                             case ConsoleKey.RightArrow: { current.Xpos++; break; }
                             case ConsoleKey.Z: { Rotate(0); break; }
                             case ConsoleKey.X: { Rotate(1); break; }
-                            case ConsoleKey.Escape: { State.paused = !State.paused; break; }
+                            case ConsoleKey.Escape: { State.Paused = !State.Paused; break; }
                             default: { break; }
                         }
                     }
@@ -85,26 +86,28 @@ namespace ConsoleTetris {
             //        acc = 0;
             //    }
             //}
-            
+
             Thread drawThread = new Thread(() => Draw(field, current));
-            Thread controlThread = new Thread(() => Controls());
-            
             drawThread.Start();
+            Thread controlThread = new Thread(() => Controls());
             controlThread.Start();
 
-            while (!State.lost) {
-                if (!State.paused) { 
-                    if (current.Ypos + 1 < 15) { current.Ypos++; } 
-                    else {
-                        for (int y = 0; y < 4; y++) { for (int x = 0; x < 4; x++) { 
-                            if (current.Shape[x, y] != 0) {
-                                field[(current.Xpos + x >= 0 && current.Xpos + x <= 9 ? current.Xpos + x : 0), 
-                                    (current.Ypos + y >= 0 && current.Ypos + y <= 15 ? current.Ypos + y : 0)] = current.Shape[x, y];
-                                }
-                            } 
-                        }
-                        current.genShape(random.Next(6));
+            while (!State.Lost) {
+                if (!State.Paused) {
+                    if (!active) {
+                        current.GenShape(random.Next(6));
                         current.Xpos = 0; current.Ypos = 0;
+                    }
+                    if (current.Ypos + 1 < 15) { current.Ypos++; active = true; } else {
+                        active = false;
+                        for (int y = 0; y < 4; y++) {
+                            for (int x = 0; x < 4; x++) {
+                                if (current.Shape[x, y] != 0) {
+                                    field[(current.Xpos + x >= 0 && current.Xpos + x <= 9 ? current.Xpos + x : 0),
+                                        (current.Ypos + y >= 0 && current.Ypos + y <= 15 ? current.Ypos + y : 0)] = current.Shape[x, y];
+                                }
+                            }
+                        }
                     }
                     Thread.Sleep(1000 / difficulty); //speed up later to up difficulty
                 }
@@ -114,27 +117,28 @@ namespace ConsoleTetris {
         }
         public static void Draw(int[,] field, Tetromino current) { //TODO: pass the current tetromino into this thread somehow
             while (true) {
-                Thread.Sleep(20); //since drawing to console takes roughly 0.25s, delay the thread to reduce flickering
+                Thread.Sleep(25); //since drawing to console takes roughly 0.25s, delay the thread to reduce flickering
                 Console.SetCursorPosition(0, 0); //return cursor to top left
                 string toWrite = "";
-                for (int y = 0; y < 16; y++) { 
-                    for (int x = 0; x < 10; x++) { 
-                        toWrite = (field[x, y] == 0 ? ".." : "[]"); 
-                        Console.Write(toWrite); 
+                for (int y = 0; y < 16; y++) {
+                    for (int x = 0; x < 10; x++) {
+                        toWrite = (field[x, y] == 0 ? ".." : "[]");
+                        Console.Write(toWrite);
                     }
                     Console.WriteLine(toWrite);
                 }
                 Console.SetCursorPosition(current.Xpos * 2, current.Ypos); //move cursor to current tetromino position
-                if (!State.paused) {
-                    for (int y = 0; y < 4; y++) { for (int x = 0; x < 4; x++) {
-                            if (current.Shape[x, y] == 0) { 
-                                Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop); 
+                if (!State.Paused) {
+                    for (int y = 0; y < 4; y++) {
+                        for (int x = 0; x < 4; x++) {
+                            if (current.Shape[x, y] == 0) {
+                                Console.SetCursorPosition(Console.CursorLeft + 2, Console.CursorTop);
                             } else { Console.Write("[]"); }
                         }
                         Console.SetCursorPosition(current.Xpos * 2, current.Ypos + 1);
                     }
                 }
-                if (State.paused) { Console.SetCursorPosition(7, 8); Console.Write("{PAUSED}"); } //draw the pause screen dialog TODO: fix since it broke somehow
+                if (State.Paused) { Console.SetCursorPosition(7, 8); Console.Write("{PAUSED}"); } //draw the pause screen dialog TODO: fix since it broke somehow
             }
         }
     }
